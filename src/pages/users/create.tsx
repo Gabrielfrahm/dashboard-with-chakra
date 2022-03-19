94b5125324b264d355b5443bc8e4d8335a926ee7
Gabriel Marques Frahm <gabriel_frahm@hotmail.com>
2022-03-19 16:04:37 -0300

    initial users

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,17 @@
+import { Box, Flex } from "@chakra-ui/react";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function CreateUser() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box></Box>
+      </Flex>
+    </Box>
+  );
+}
